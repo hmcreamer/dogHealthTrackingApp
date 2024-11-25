7af2a53876d2b6d92dfe0160aa2abcdcce2f144c
Henry Creamer <bcards4me@verizon.net>
2024-11-24 22:24:19 -0600

    Refactored home page view to be separated out from ContentView

--- DogHealthTracker/Views/ContentView.swift
+++ DogHealthTracker/Views/ContentView.swift
@@ -9,60 +9,8 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Dog.name, ascending: true)],
-        animation: .default)
-    private var dogs: FetchedResults<Dog>
-    @State private var isAddingDogProfile = false
-
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(dogs) { dog in
-                    NavigationLink {
-                        Text("\(dog.name!)")
-                    } label: {
-                        Text(dog.name!)
-                    }
-                }
-                .onDelete(perform: deleteDog)
-            }
-            .navigationTitle("Dog Profiles")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: {
-                        isAddingDogProfile.toggle()
-                    }) {
-                        Label("Add Dog", systemImage: "plus")
-                    }
-                }
-            }
-            .sheet(isPresented: $isAddingDogProfile) {
-                AddDogFormView(isPresented: $isAddingDogProfile)
-            }
-        } detail: {
-            Text("Select a dog")
-        }
-    }
-
-    private func deleteDog(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { dogs[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+        HomePageView()
     }
 }
 
