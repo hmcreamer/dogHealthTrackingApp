eeab49096a33a1b930744a578a39152e3c9664db
Henry Creamer <bcards4me@verizon.net>
2025-01-14 21:48:55 -0600

    Fixed model for MedicalEvent

--- DogHealthTracker/Data/Persistence.swift
+++ DogHealthTracker/Data/Persistence.swift
@@ -25,17 +25,17 @@ struct PersistenceController {
         newDog.weight = Int32(25)
         newDog.lastVetVisit = Calendar.current.date(byAdding: .month, value: -2, to: Date())
         
+        let event1 = MedicalEvent(context: viewContext)
+         event1.eventDescription = "Rabies Vaccine"
+         event1.occurrenceDate = Date()
+         event1.expirationDate = Calendar.current.date(byAdding: .year, value: 1, to: Date())
+         event1.reminderDate = Calendar.current.date(byAdding: .month, value: 11, to: Date())
+         event1.type = "Vaccine"
+         event1.dog = newDog
+        
         let pdfDoc = PDFDoc(context: viewContext)
         pdfDoc.title = "Sample Medical Record"
         pdfDoc.dog = newDog
-        
-        let event1 = MedicalEvent(context: viewContext)
-        event1.eventDescription = "Rabies Vaccine"
-        event1.occurrenceDate = Date()
-        event1.expirationDate = Calendar.current.date(byAdding: .year, value: 1, to: Date())
-        event1.reminderDate = Calendar.current.date(byAdding: .month, value: 11, to: Date())
-        event1.typeData = "Vaccine"
-        event1.dog = newDog
 
         // Get the URL of the Sample2.pdf file from the app's bundle
         if let samplePDFURL = Bundle.main.url(forResource: "Sample2", withExtension: "pdf") {
